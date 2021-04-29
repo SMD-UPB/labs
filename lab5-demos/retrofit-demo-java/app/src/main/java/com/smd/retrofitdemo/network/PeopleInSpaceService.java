7fc843850c9de522cef8e214b077d5ca39723544
Alex Chirvase <alexandru.chirvase@bullguard.com>
2021-04-29 21:10:37 +0300

    Move from lab5-demos to demos

--- lab5-demos/retrofit-demo-java/app/src/main/java/com/smd/retrofitdemo/network/PeopleInSpaceService.java
+++
@@ -1,9 +0,0 @@
-package com.smd.retrofitdemo.network;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface PeopleInSpaceService {
-    @GET("/astros.json")
-    Call<PeopleInSpace> getPeopleInSpace();
-}
--- lab5-demos/retrofit-demo-kotlin/app/src/main/java/com/smd/retrofitdemo/network/PeopleInSpaceService.kt
+++
@@ -1,9 +0,0 @@
-package com.smd.retrofitdemo.network
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface PeopleInSpaceService {
-    @GET("/astros.json")
-    fun peopleInSpace(): Call<PeopleInSpace>
-}
